22707b158d5c446397da80d347c41327daaad396
folex <0xdxdy@gmail.com>
2021-07-01 22:23:04 +0300

    upload file to ipfs

--- aqua/typescript/index.ts
+++ aqua/typescript/index.ts
@@ -14,19 +14,58 @@
  * limitations under the License.
  */
 
-export * from './ipfs-api';
+import { put, get_from } from './ipfs-api';
 
 import {createClient, registerServiceFunction, setLogLevel, FluenceClient} from "@fluencelabs/fluence";
-import {krasnodar} from "@fluencelabs/fluence-network-environment";
+import {krasnodar, Node} from "@fluencelabs/fluence-network-environment";
+const { create, globSource, urlSource } = require('ipfs-http-client');
+const all = require('it-all');
+const uint8ArrayConcat = require('uint8arrays/concat')
 
 
+let local: Node[] = [
+    {
+        peerId: "12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK",
+        multiaddr: "/ip4/127.0.0.1/tcp/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK"
+    },
+    {
+        peerId: "12D3KooWRABanQHUn28dxavN9ZS1zZghqoZVAYtFpoN7FdtoGTFv",
+        multiaddr: "/ip4/127.0.0.1/tcp/9991/ws/p2p/12D3KooWRABanQHUn28dxavN9ZS1zZghqoZVAYtFpoN7FdtoGTFv"
+    },
+    {
+        peerId: "12D3KooWFpQ7LHxcC9FEBUh3k4nSCC12jBhijJv3gJbi7wsNYzJ5",
+        multiaddr: "/ip4/127.0.0.1/tcp/9992/ws/p2p/12D3KooWFpQ7LHxcC9FEBUh3k4nSCC12jBhijJv3gJbi7wsNYzJ5"
+    },
+];
 
 async function main() {
     // setLogLevel('DEBUG');
-    const client = await createClient(krasnodar[1]);
-    console.log("created a client %s with relay %s", client.selfPeerId, client.relayPeerId);
+    const fluence = await createClient(local[1]);
+    console.log("created a fluence client %s with relay %s", fluence.selfPeerId, fluence.relayPeerId);
     
-    
+    let ipfsMultiaddr = 'http://127.0.0.1:5001';
+    const ipfs = create(ipfsMultiaddr);
+    console.log("created ipfs client");
+
+    await ipfs.id();
+    console.log("connected to ipfs");
+
+    let source = urlSource('https://miro.medium.com/max/1172/0*4HuRgZhewW30VU0x.png');
+    const file = await ipfs.add(source);
+    console.log("uploaded file:", file);
+
+    let files = await ipfs.get(file.cid);
+    for await (const file of files) {
+        const content = uint8ArrayConcat(await all(file.content));
+        console.log("downloaded file of length ", content.length);
+    }
+
+    console.log("file hash: ", file.cid.value);
+    let getResult = await get_from(fluence, local[2].peerId, file.cid.value, ipfsMultiaddr, { ttl: 10000 });
+    console.log("Ipfs.get", getResult);
+
+    let putResult = await put(fluence, local[2].peerId, { ttl: 10000 });
+    console.log("Ipfs.put", putResult);
 
     return;
 }
--- aqua/typescript/ipfs-api.ts
+++ aqua/typescript/ipfs-api.ts
@@ -12,9 +12,9 @@ import { RequestFlow } from '@fluencelabs/fluence/dist/internal/RequestFlow';
 
 
 
-export async function put(client: FluenceClient, node: string, config?: {ttl?: number}): Promise<void> {
+export async function put(client: FluenceClient, node: string, config?: {ttl?: number}): Promise<{result:string;success:boolean}> {
     let request: RequestFlow;
-    const promise = new Promise<void>((resolve, reject) => {
+    const promise = new Promise<{result:string;success:boolean}>((resolve, reject) => {
         request = new RequestFlowBuilder()
             .disableInjections()
             .withTTL(config?.ttl || 5000)
@@ -24,23 +24,29 @@ export async function put(client: FluenceClient, node: string, config?: {ttl?: n
  (seq
   (seq
    (seq
-    (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
-    (call %init_peer_id% ("getDataSrv" "node") [] node)
+    (seq
+     (seq
+      (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+      (call %init_peer_id% ("getDataSrv" "node") [] node)
+     )
+     (call -relay- ("op" "noop") [])
+    )
+    (xor
+     (call node ("ipfs-adapter" "put") ["/tmp/test"] result)
+     (seq
+      (call -relay- ("op" "noop") [])
+      (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+     )
+    )
    )
    (call -relay- ("op" "noop") [])
   )
   (xor
-   (call node ("ipfs" "put") ["/tmp/test"])
-   (seq
-    (call -relay- ("op" "noop") [])
-    (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
-   )
+   (call %init_peer_id% ("callbackSrv" "response") [result])
+   (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
   )
  )
- (seq
-  (call -relay- ("op" "noop") [])
-  (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
- )
+ (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 3])
 )
 
             `,
@@ -50,7 +56,11 @@ export async function put(client: FluenceClient, node: string, config?: {ttl?: n
                     return client.relayPeerId!;
                 });
                 h.on('getDataSrv', 'node', () => {return node;});
-                
+                h.onEvent('callbackSrv', 'response', (args) => {
+  const [res] = args;
+  resolve(res);
+});
+
                 h.onEvent('errorHandlingSrv', 'error', (args) => {
                     // assuming error is the single argument
                     const [err] = args;
@@ -64,6 +74,81 @@ export async function put(client: FluenceClient, node: string, config?: {ttl?: n
             .build();
     });
     await client.initiateFlow(request!);
-    return Promise.race([promise, Promise.resolve()]);
+    return promise;
+}
+      
+
+
+export async function get_from(client: FluenceClient, node: string, hash: string, from: string, config?: {ttl?: number}): Promise<{result:string;success:boolean}> {
+    let request: RequestFlow;
+    const promise = new Promise<{result:string;success:boolean}>((resolve, reject) => {
+        request = new RequestFlowBuilder()
+            .disableInjections()
+            .withTTL(config?.ttl || 5000)
+            .withRawScript(
+                `
+(xor
+ (seq
+  (seq
+   (seq
+    (seq
+     (seq
+      (seq
+       (seq
+        (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+        (call %init_peer_id% ("getDataSrv" "node") [] node)
+       )
+       (call %init_peer_id% ("getDataSrv" "hash") [] hash)
+      )
+      (call %init_peer_id% ("getDataSrv" "from") [] from)
+     )
+     (call -relay- ("op" "noop") [])
+    )
+    (xor
+     (call node ("ipfs-adapter" "get_from") [hash from] result)
+     (seq
+      (call -relay- ("op" "noop") [])
+      (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+     )
+    )
+   )
+   (call -relay- ("op" "noop") [])
+  )
+  (xor
+   (call %init_peer_id% ("callbackSrv" "response") [result])
+   (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+  )
+ )
+ (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 3])
+)
+
+            `,
+            )
+            .configHandler((h) => {
+                h.on('getDataSrv', '-relay-', () => {
+                    return client.relayPeerId!;
+                });
+                h.on('getDataSrv', 'node', () => {return node;});
+h.on('getDataSrv', 'hash', () => {return hash;});
+h.on('getDataSrv', 'from', () => {return from;});
+                h.onEvent('callbackSrv', 'response', (args) => {
+  const [res] = args;
+  resolve(res);
+});
+
+                h.onEvent('errorHandlingSrv', 'error', (args) => {
+                    // assuming error is the single argument
+                    const [err] = args;
+                    reject(err);
+                });
+            })
+            .handleScriptError(reject)
+            .handleTimeout(() => {
+                reject('Request timed out for get_from');
+            })
+            .build();
+    });
+    await client.initiateFlow(request!);
+    return promise;
 }
       
