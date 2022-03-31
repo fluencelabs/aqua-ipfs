0beabf1586bc5da434d5fb6dc008787a7b0b7290
folex <0xdxdy@gmail.com>
2022-03-31 22:33:35 +0400

    Rust update WIP

--- service/pure/tests/tests.rs
+++ service/pure/tests/tests.rs
@@ -20,42 +20,48 @@ mod tests {
 
     macro_rules! set_default_local_api_multiaddr {
         ($ipfs_pure:expr) => {
-            let result = $ipfs_pure.set_local_api_multiaddr("/ip4/127.0.0.1/tcp/9992".to_string());
+            let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
+            let result = ipfs_pure.set_local_api_multiaddr("/ip4/127.0.0.1/tcp/9992".to_string());
             assert!(result.success);
         }
     }
 
-    #[marine_test(config_path = "Config.toml", modules_dir = "../../artifacts")]
+    #[marine_test(ipfs_pure(config_path = "Config.toml", modules_dir = "../../artifacts"))]
     fn invalid_multiaddr() {
         let invalid_multiaddr = "invalid_multiaddr".to_string();
+        let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
         let result = ipfs_pure.set_local_api_multiaddr(invalid_multiaddr.clone());
         assert!(!result.success);
         assert_eq!(format!("invalid multiaddr: {}", invalid_multiaddr), result.error);
     }
 
-    #[marine_test(config_path = "Config.toml", modules_dir = "../../artifacts")]
+    #[marine_test(ipfs_pure(config_path = "Config.toml", modules_dir = "../../artifacts"))]
     fn set_get_external_api_multiaddr() {
         set_default_local_api_multiaddr!(ipfs_pure);
         let multiaddr = "/ip4/127.0.0.1/tcp/9992";
+        let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
         let result = ipfs_pure.set_external_api_multiaddr(multiaddr.to_string());
         assert!(result.success);
 
-        let peer_id = ipfs_effector.get_peer_id("/ip4/127.0.0.1/tcp/5001".to_string(), 0).peer_id;
+        let peer_id = ipfs_pure.get_peer_id("/ip4/127.0.0.1/tcp/5001".to_string(), 0).peer_id;
 
+        let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
         let result = ipfs_pure.get_external_api_multiaddr();
         assert!(result.success);
         assert_eq!(format!("{}/p2p/{}", multiaddr, peer_id), result.multiaddr);
     }
 
-    #[marine_test(config_path = "Config.toml", modules_dir = "../../artifacts")]
+    #[marine_test(ipfs_pure(config_path = "Config.toml", modules_dir = "../../artifacts"))]
     fn set_get_external_swarm_multiaddr() {
         set_default_local_api_multiaddr!(ipfs_pure);
         let multiaddr = "/ip4/127.0.0.1/tcp/9992";
+        let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
         let result = ipfs_pure.set_external_swarm_multiaddr(multiaddr.to_string());
         assert!(result.success);
 
-        let peer_id = ipfs_effector.get_peer_id("/ip4/127.0.0.1/tcp/5001".to_string(), 0).peer_id;
+        let peer_id = ipfs_pure.get_peer_id("/ip4/127.0.0.1/tcp/5001".to_string(), 0).peer_id;
 
+        let mut ipfs_pure = marine_test_env::ipfs_pure::ServiceInterface::new();
         let result = ipfs_pure.get_external_swarm_multiaddr();
         assert!(result.success);
         assert_eq!(format!("{}/p2p/{}", multiaddr, peer_id), result.multiaddr);
