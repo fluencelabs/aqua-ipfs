0035819178ef123ca312bed8714397a94b30d32f
Valery Antopol <valery.antopol@gmail.com>
2022-06-28 17:39:06 +0300

    Add workaround for marine memory leak

--- service/pure/src/main.rs
+++ service/pure/src/main.rs
@@ -16,11 +16,36 @@
 
 #![feature(try_blocks)]
 
+/*
+    _initialize function that calls __wasm_call_ctors is required to mitigade memory leak
+    that is described in https://github.com/WebAssembly/wasi-libc/issues/298
+
+    In short, without this code rust wraps every export function
+    with __wasm_call_ctors/__wasm_call_dtors calls. This causes memory leaks. When compiler sees
+    an explicit call to __wasm_call_ctors in _initialize function, it disables export wrapping.
+
+    TODO: remove when updating to marine-rs-sdk with fix
+ */
+#[cfg(target_arch = "wasm32")]
+extern "C" {
+    pub fn __wasm_call_ctors();
+}
+
+#[cfg(target_arch = "wasm32")]
+#[no_mangle]
+fn _initialize() {
+    unsafe {
+        __wasm_call_ctors();
+    }
+}
+
 #[cfg(target_arch = "wasm32")]
 mod pure;
 
+
 #[cfg(target_arch = "wasm32")]
 pub fn main() {
+    _initialize(); // As __wasm_call_ctors still does necessary work, we call it at the start of the module
     pure::main()
 }
 
