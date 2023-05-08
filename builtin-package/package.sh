4ce8e8bce7f9d0b4ea046b73391f3b5199ab02a6
Aleksey Proshutisnkiy <justprosh@users.noreply.github.com>
2023-05-08 17:42:29 +0400

    feat(builtin-package): use new blueprint (#81)

--- builtin-package/package.sh
+++ builtin-package/package.sh
@@ -3,21 +3,39 @@ set -o pipefail -o nounset -o errexit
 
 # set current working directory to script directory to run script from everywhere
 cd "$(dirname "$0")"
-SCRIPT_DIR="$(pwd)"
+PACKAGE_DIR="$(pwd)/../aqua-ipfs"
+
+(
+    rm -rf $PACKAGE_DIR/*
+    mkdir -p $PACKAGE_DIR
+)
 
 (
     echo "*** copy wasm files ***"
     cd ../service
-    cp artifacts/*.wasm "$SCRIPT_DIR"
+    cp artifacts/*.wasm "$PACKAGE_DIR"
 )
+
+(
+    echo "*** copy on_start script ***"
+    cp on_start.json "$PACKAGE_DIR"
+    cp on_start.air "$PACKAGE_DIR"
+)
+
+PURE_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/ipfs_pure.wasm)
+EFFECTOR_CID=$(ipfs add -q --only-hash --cid-version=1 --chunker=size-262144 $PACKAGE_DIR/ipfs_effector.wasm)
+mv $PACKAGE_DIR/ipfs_pure.wasm "$PACKAGE_DIR"/"$PURE_CID".wasm
+mv $PACKAGE_DIR/ipfs_effector.wasm "$PACKAGE_DIR"/"$EFFECTOR_CID".wasm
+cp ipfs_pure_config.json "$PACKAGE_DIR"/"$PURE_CID"_config.json
+cp ipfs_effector_config.json "$PACKAGE_DIR"/"$EFFECTOR_CID"_config.json
+
+# write blueprint.json
+echo "{}" | jq --arg pure_cid "$PURE_CID" --arg effector_cid "$EFFECTOR_CID" '{"name": "ipfs_pure", "dependencies":[{"/":$effector_cid},{"/":$pure_cid}]}' > "$PACKAGE_DIR/blueprint.json"
 
 (
     echo "*** create builtin distribution package ***"
     cd ..
-    mv builtin-package aqua-ipfs
-    tar --exclude="package.sh" -f aqua-ipfs.tar.gz -zcvv ./aqua-ipfs
-    mv aqua-ipfs builtin-package
-    echo "*** package created ***"
+    tar -f aqua-ipfs.tar.gz -zcvv ./aqua-ipfs
 )
 
 echo "*** done ***"
